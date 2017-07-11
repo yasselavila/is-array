46fa46de69c37cb7ce313dca9eb23102face7ddb
Yassel Avila <yassel.avila@gmail.com>
2017-07-11 18:41:34 -0400

    Export members

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @yag/is-array
+ *
+ * @copyright Copyright (c) 2016-2017, Yassel Avila Gil (http://yasselavila.com)
+ * @license   BSD 3 Clause (see LICENSE.txt)
+ * @link      https://github.com/yasselavila/is-array
+ */
+
+import { default as isArray, fallbackIsArray } from './is-array';
+
+export {
+  isArray,
+  fallbackIsArray
+};
+
+export default isArray;
