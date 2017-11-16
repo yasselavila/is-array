6d14c6a97a318bc7eaa6dee0fd8aa413024a3fb0
Yassel Avila <yassel.avila@gmail.com>
2017-11-16 17:21:31 -0500

    Add mocks for testing

---
+++ src/test/mocks.ts
@@ -0,0 +1,14 @@
+/**
+ * @yag/is-array
+ *
+ * @copyright Copyright (c) 2016-2017, Yassel Avila Gil (http://yasselavila.com)
+ * @license   BSD 3 Clause (see LICENSE.txt)
+ * @link      https://github.com/yasselavila/is-array
+ */
+
+export const t1: any = new Array();
+export const t2: any = [];
+export const t3: any = [1, 2, 3];
+export const t4: any = {};
+export const t5: any = new Date();
+export const t6: any = /^[1-9]+$/g;
