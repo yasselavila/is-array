16ee38003afc0248ec2b966838f9af38460cf682
Yassel Avila <yassel.avila@gmail.com>
2017-11-18 14:24:23 -0500

    Fix copyright year

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * @yag/is-array
  *
- * @copyright Copyright (c) 2016-2017, Yassel Avila Gil (http://yasselavila.com)
+ * @copyright Copyright (c) 2017, Yassel Avila Gil (http://yasselavila.com)
  * @license   BSD 3 Clause (see LICENSE.txt)
  * @link      https://github.com/yasselavila/is-array
  */
